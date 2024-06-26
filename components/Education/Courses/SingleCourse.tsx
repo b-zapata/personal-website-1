d61920eda81ecdf52ea92dfbae6269d51066e271
b-zapata <b.zapata.n@gmail.com>
2024-06-26 15:46:57 -0600

    testing a tag positioning

--- components/Education/Courses/SingleCourse.tsx
+++ components/Education/Courses/SingleCourse.tsx
@@ -8,29 +8,29 @@ const SingleCourse = ({ course }: { course: Course }) => {
 
   return (
     <>
-      <a
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="block"
-      >
-        <motion.div
-          variants={{
-            hidden: {
-              opacity: 0,
-              y: -10,
-            },
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            y: -10,
+          },
 
-            visible: {
-              opacity: 1,
-              y: 0,
-            },
-          }}
-          initial="hidden"
-          whileInView="visible"
-          transition={{ duration: 0.5 }}
-          viewport={{ once: true }}
-          className="animate_top z-40 rounded-lg border border-white bg-white p-7.5 shadow-solid-3 transition-all hover:shadow-solid-4 dark:border-strokedark dark:bg-blacksection dark:hover:bg-hoverdark xl:p-12.5"
+          visible: {
+            opacity: 1,
+            y: 0,
+          },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        className="animate_top z-40 rounded-lg border border-white bg-white p-7.5 shadow-solid-3 transition-all hover:shadow-solid-4 dark:border-strokedark dark:bg-blacksection dark:hover:bg-hoverdark xl:p-12.5"
+      >
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block"
         >
           <h3 className="mb-5 mt-7.5 text-xl font-semibold text-black dark:text-white xl:text-itemtitle">
             {title}
@@ -39,8 +39,8 @@ const SingleCourse = ({ course }: { course: Course }) => {
             {subtitle}
           </h5>
           <p>{description}</p>
-        </motion.div>
-      </a>
+        </a>
+      </motion.div>
     </>
   );
 };
