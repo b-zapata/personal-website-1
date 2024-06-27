f031056c5acc8e7351e48a61bf6c497d88e44ade
b-zapata <b.zapata.n@gmail.com>
2024-06-27 11:50:34 -0600

    got rid of timeline in experience tab

--- components/Experience/ExperienceTab/ExperiencesTabItem.tsx
+++ components/Experience/ExperienceTab/ExperiencesTabItem.tsx
@@ -12,12 +12,6 @@ const ExperiencesTabItem = ({
 
   return (
     <>
-      <div>
-        <main className="min-h-screen bg-gray-100 py-10">
-          <h1 className="mb-10 text-center text-4xl font-bold">My Timeline</h1>
-          <Timeline />
-        </main>
-      </div>
       <div className="flex items-center gap-8 lg:gap-19">
         <div className="md:w-1/2">
           <h2 className="mb-7 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle2">
