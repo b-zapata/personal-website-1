73be8be49acf8ed416b28527ad636ca3d3671d48
b-zapata <b.zapata.n@gmail.com>
2024-06-26 21:08:11 -0600

    testing out new index component on experience - jobs

--- components/Timeline/index.tsx
+++ components/Timeline/index.tsx
@@ -1,36 +1,102 @@
 import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Timeline = () => {
-  const events = [
+  const jobs = [
     {
-      date: "2021-09-01",
-      title: "Started University",
-      description: "Enrolled in Computer Science at XYZ University.",
+      startDate: "Jul 2023",
+      endDate: "Present",
+      title: "Computer Staff Representative",
+      employer: "Brigham Young University - Math Department",
+      location: "Hermosillo, Mexico",
+      bulletPoint1:
+        "Architeted new math major website to improve traffic by 30%",
+      bulletPoint2:
+        "Modernize department equipment to ensure up-to-date technology for faculty and students",
+      bulletPoint3:
+        "Offer on-demand tech support to faculty and students to enhance learning experience",
     },
     {
-      date: "2022-06-15",
-      title: "Internship at ABC Corp",
-      description: "Completed a summer internship at ABC Corporation.",
+      startDate: "Feb 2021",
+      endDate: "Mar 2023",
+      title: "Volunteer Representative",
+      employer: "The Church of Jesus Christ of Latter-day Saints",
+      location: "Santiago, Chile & Rio de Janeiro, Brazil",
+      bulletPoint1:
+        "Averaged 70+ work hours a week teaching life skills to people from various backgrounds",
+      bulletPoint2:
+        "Served in leadership positions of increasing scope to boost missionary results and training",
+      bulletPoint3:
+        "Fulfilled key administrative and logistical responsibilities to ensure well-being of 150+ missionaries",
     },
     {
-      date: "2023-01-10",
-      title: "Joined OpenAI",
-      description: "Started working at OpenAI as a Developer.",
+      startDate: "Mar 2020",
+      endDate: "Dec 2020",
+      title: "Mendix Developer",
+      employer: "Brigham Young University - Student Life Technology",
+      location: "Provo, UT",
+      bulletPoint1:
+        "Architected 3 department websites with API integrations to Strava and other platforms to improve user experience",
+      bulletPoint2:
+        "Conducted regular consultations with stakeholders to align website development with their expectations",
+      bulletPoint3:
+        "Pioneered development in the Mendix platform to offer personalized training to 2 team members",
     },
   ];
 
   return (
-    <div className="container mx-auto my-10 p-5">
-      <div className="relative border-l-2 border-gray-300">
-        {events.map((event, index) => (
-          <div key={index} className="mb-8 ml-4">
-            <div className="absolute -left-3 mt-1.5 h-6 w-6 rounded-full bg-blue-500"></div>
-            <p className="text-sm text-gray-500">{event.date}</p>
-            <h4 className="mb-1 text-lg font-semibold">{event.title}</h4>
-            <p className="text-gray-700">{event.description}</p>
+    <div>
+      {jobs.map((job, index) => (
+        <div
+          key={index}
+          className="flex flex-wrap items-center justify-center gap-8"
+        >
+          <Image
+            src="/images/logo/BYU-logo.png"
+            alt="BYU Logo"
+            width={150}
+            height={150}
+            className="rounded-lg"
+          />
+          {/* <!-- Card begins here --> */}
+          <div
+            key={index}
+            className="w-full max-w-[1200px] rounded-lg bg-gradient-to-t from-[#F8F9FF] to-[#DEE7FF] px-7.5 py-15 dark:bg-blacksection dark:bg-gradient-to-t dark:from-transparent dark:to-transparent dark:stroke-strokedark md:px-12.5 xl:px-17.5 xl:py-12"
+          >
+            <div className="flex flex-wrap gap-8 md:flex-nowrap md:items-center md:justify-between md:gap-0">
+              <motion.div
+                variants={{
+                  hidden: {
+                    opacity: 0,
+                    x: -20,
+                  },
+
+                  visible: {
+                    opacity: 1,
+                    x: 0,
+                  },
+                }}
+                initial="hidden"
+                whileInView="visible"
+                transition={{ duration: 1, delay: 0.1 }}
+                viewport={{ once: true }}
+                className="animate_left w-full"
+              >
+                <h2 className="xl:text-sectionsubtitle mb-4 w-11/12 text-3xl font-bold text-black dark:text-white">
+                  {job.title}
+                </h2>
+                <h3 className="xl:text-sectionsubtitle mb-5 w-11/12 text-xl font-bold text-black dark:text-white">
+                  {job.employer}
+                </h3>
+                <p className="my-5">⚡ {job.bulletPoint1}</p>
+                <p className="my-5">⚡ {job.bulletPoint2}</p>
+                <p className="my-5">⚡ {job.bulletPoint3}</p>
+              </motion.div>
+            </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
