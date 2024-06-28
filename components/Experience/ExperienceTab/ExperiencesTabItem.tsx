141ffd34256032159407462a99d7ab45d16ba437
b-zapata <b.zapata.n@gmail.com>
2024-06-28 11:00:49 -0600

    testing conditional show of bullet points in experience tabs

--- components/Experience/ExperienceTab/ExperiencesTabItem.tsx
+++ components/Experience/ExperienceTab/ExperiencesTabItem.tsx
@@ -9,7 +9,7 @@ const ExperiencesTabItem = ({
 }: {
   experienceTab: ExperienceTab;
 }) => {
-  const { experiences } = experienceTab;
+  const { id, experiences } = experienceTab;
 
   return (
     <>
@@ -19,13 +19,33 @@ const ExperiencesTabItem = ({
             key={index}
             className="flex flex-wrap items-center justify-center gap-8 py-10"
           >
-            <Image
-              src={experience.logo}
-              alt="BYU Logo"
-              width={150}
-              height={150}
-              className="rounded-lg"
-            />
+            <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  x: -20,
+                },
+
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 1, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_left w-full"
+            >
+              <Image
+                src={experience.logo}
+                alt="BYU Logo"
+                width={150}
+                height={150}
+                className="rounded-lg"
+              />
+            </motion.div>
+
             {/* <!-- Card begins here --> */}
             <div
               key={index}
@@ -56,9 +76,16 @@ const ExperiencesTabItem = ({
                   <h3 className="xl:text-sectionsubtitle mb-5 w-11/12 text-xl font-bold text-black dark:text-white">
                     {experience.employer}
                   </h3>
-                  <p className="my-5">⚡ {experience.bulletPoint1}</p>
-                  <p className="my-5">⚡ {experience.bulletPoint2}</p>
-                  <p className="my-5">⚡ {experience.bulletPoint3}</p>
+                  {/* Conditional rendering based on bulletPoint existence */}
+                  {experience.bulletPoint1 && (
+                    <p className="my-5">⚡ {experience.bulletPoint1}</p>
+                  )}
+                  {experience.bulletPoint2 && (
+                    <p className="my-5">⚡ {experience.bulletPoint2}</p>
+                  )}
+                  {experience.bulletPoint3 && (
+                    <p className="my-5">⚡ {experience.bulletPoint3}</p>
+                  )}
                 </motion.div>
               </div>
             </div>
@@ -66,32 +93,6 @@ const ExperiencesTabItem = ({
         ))}
       </div>
     </>
-
-    /*
-  const { title, desc1, desc2, image, imageDark } = experienceTab;
-
-  return (
-    <>
-      <div className="flex items-center gap-8 lg:gap-19">
-        <div className="md:w-1/2">
-          <h2 className="mb-7 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle2">
-            {title}
-          </h2>
-          <p className="mb-5">{desc1}</p>
-          <p className="w-11/12">{desc2}</p>
-        </div>
-        <div className="relative mx-auto hidden aspect-[562/366] max-w-[550px] md:block md:w-1/2">
-          <Image src={image} alt={title} fill className="dark:hidden" />
-          <Image
-            src={imageDark}
-            alt={title}
-            fill
-            className="hidden dark:block"
-          />
-        </div>
-      </div>
-    </>
-    */
   );
 };
 
