2a29da9e28e6819e3b326e7995702d282e7f36c0
b-zapata <b.zapata.n@gmail.com>
2024-07-01 14:46:32 -0600

    ordered projects

--- components/Projects/ProjectsTab/projectsTabData.tsx
+++ components/Projects/ProjectsTab/projectsTabData.tsx
@@ -5,6 +5,157 @@ const projectsTabData: ProjectTab[] = [
     id: "tabOne",
     projects: [
       {
+        title: "INTEX II - Online Lego Store",
+        description: `This was the culminating project of the junior core of the BYU Information Systems program, part of the Integrated Experience (INTEX) series. As part of a team of four students that I had been working with throughout the semester on various projects, we developed an online retail store for a fictional client, Aurora Brickwell, who had been selling Lego products on social media but wanted a more organized platform. Some of the main features we included were: \n
+		⚡ Cart and checkout functionalities \n
+		⚡ Product recommendations based on machine learning algorithms \n
+		⚡ Admin capabilities for adding, removing and editing users and products \n
+		⚡ Two-factor authentication \n
+		⚡ Protection from common cyberattacks (such as cross-site scripting and SQL injection) \n
+	  We received the project prompt on Monday morning, worked 60+ hours during the week following the Scrum framework, and presented our work to faculty and industry professionals on Friday afternoon.`,
+        codingStacks: [
+          {
+            title: "HTML",
+            iconLight: "",
+            iconDark: "images/icon/stacks/html-icon.png",
+          },
+          {
+            title: "CSS",
+            iconLight: "",
+            iconDark: "images/icon/stacks/css-icon.png",
+          },
+          {
+            title: "Python",
+            iconLight: "",
+            iconDark: "images/icon/stacks/python-icon.png",
+          },
+          {
+            title: "JavaScript",
+            iconLight: "",
+            iconDark: "images/icon/stacks/js-icon.png",
+          },
+          {
+            title: "C Sharp",
+            iconLight: "",
+            iconDark: "images/icon/stacks/c-sharp-icon.png",
+          },
+          {
+            title: "SQL",
+            iconLight: "",
+            iconDark: "images/icon/stacks/sql-icon.png",
+          },
+          {
+            title: "Microsoft Azure",
+            iconLight: "",
+            iconDark: "images/icon/stacks/azure-icon.png",
+          },
+          {
+            title: "Figma",
+            iconLight: "",
+            iconDark: "images/icon/stacks/figma-2-icon.png",
+          },
+          {
+            title: "Lucidchart",
+            iconLight: "",
+            iconDark: "images/icon/stacks/lucidchart-2-icon.png",
+          },
+        ],
+        link: "",
+      },
+      {
+        title: "INTEX I - Social Media Usage and Mental Health Dashboard",
+        description: `This was the culminating project of the first semester of the BYU Information Systems junior core. As part of a team of four students that I had been working with throughout the semester on various projects, we developed a professional dashboard for the Provo City Council (fictional client) that showed the relationship between mental health and social media usage. Some of the main features we included were: \n
+		○ Form submission functionality for new people being surveyed \n
+		○ Admin functionalities for adding, editing, and removing data \n
+		○ Real-time update of dashboards as data was added, removed, or edited \n
+		○ Basic log in functionalities \n
+		○ Professional data visualizations following best industry practices`,
+        codingStacks: [
+          {
+            title: "HTML",
+            iconLight: "",
+            iconDark: "images/icon/stacks/html-icon.png",
+          },
+          {
+            title: "CSS",
+            iconLight: "",
+            iconDark: "images/icon/stacks/css-icon.png",
+          },
+          {
+            title: "Python",
+            iconLight: "",
+            iconDark: "images/icon/stacks/python-icon.png",
+          },
+          {
+            title: "JavaScript",
+            iconLight: "",
+            iconDark: "images/icon/stacks/js-icon.png",
+          },
+          {
+            title: "NodeJS",
+            iconLight: "",
+            iconDark: "images/icon/stacks/nodejs-icon.png",
+          },
+          {
+            title: "SQL",
+            iconLight: "",
+            iconDark: "images/icon/stacks/sql-icon.png",
+          },
+          {
+            title: "Amazon Web Services",
+            iconLight: "images/icon/stacks/aws-light-icon.png",
+            iconDark: "images/icon/stacks/aws-dark-2-icon.png",
+          },
+          {
+            title: "Lucidchart",
+            iconLight: "",
+            iconDark: "images/icon/stacks/lucidchart-2-icon.png",
+          },
+        ],
+        link: "",
+      },
+      {
+        title: "ToaPro",
+        description: `Following the Nexus development framework, I worked in a team of 60 students of my cohort to create an app for professors to use to grade the INTEX series. Some of the main features implemented were: \n
+		⚡ Project submission for student teams \n
+		⚡ Grid view of submissions for teaching assistants and professors \n
+		⚡ Grading functionality for graders, visible to students upon completion of the grading process
+`,
+        codingStacks: [
+          {
+            title: "HTML",
+            iconLight: "",
+            iconDark: "images/icon/stacks/html-icon.png",
+          },
+          {
+            title: "CSS",
+            iconLight: "",
+            iconDark: "images/icon/stacks/css-icon.png",
+          },
+          {
+            title: "C Sharp",
+            iconLight: "",
+            iconDark: "images/icon/stacks/c-sharp-icon.png",
+          },
+          {
+            title: "SQL",
+            iconLight: "",
+            iconDark: "images/icon/stacks/sql-icon.png",
+          },
+          {
+            title: "Figma",
+            iconLight: "",
+            iconDark: "images/icon/stacks/figma-2-icon.png",
+          },
+        ],
+        link: "",
+      },
+    ],
+  },
+  {
+    id: "tabTwo",
+    projects: [
+      {
         title: "NiteOut",
         description:
           "A mobile app that finds parties and other events nearby. Aimed at a college-aged audience, it drops pins on the map with the nearest relevant parties based on filters defined by the user. Users can create parties and look for others nearby.",
@@ -96,7 +247,7 @@ const projectsTabData: ProjectTab[] = [
     ],
   },
   {
-    id: "tabTwo",
+    id: "tabThree",
     projects: [
       {
         title: "Lazy Ironman Registration & Tracking",
@@ -133,158 +284,6 @@ const projectsTabData: ProjectTab[] = [
         description:
           "A coworker and I revamped the current BYU Math department website to give it a professional, BYU-themed look. We used the Brighstpot platform.",
         codingStacks: [],
-        link: "",
-      },
-    ],
-  },
-  {
-    id: "tabThree",
-    projects: [
-      {
-        title: "INTEX II - Online Lego Store",
-        description: `This was the culminating project of the junior core of the BYU Information Systems program, part of the Integrated Experience (INTEX) series. As part of a team of four students that I had been working with throughout the semester on various projects, we developed an online retail store for a fictional client, Aurora Brickwell, who had been selling Lego products on social media but wanted a more organized platform. Some of the main features we included were: \n
-		⚡ Cart and checkout functionalities \n
-		⚡ Product recommendations based on machine learning algorithms \n
-		⚡ Admin capabilities for adding, removing and editing users and products \n
-		⚡ Two-factor authentication \n
-		⚡ Protection from common cyberattacks (such as cross-site scripting and SQL injection) \n
-	We received the project prompt on Monday morning, worked 60+ hours during the week following the Scrum framework, and presented our work to faculty and industry professionals on Friday afternoon.`,
-        codingStacks: [
-          {
-            title: "HTML",
-            iconLight: "",
-            iconDark: "images/icon/stacks/html-icon.png",
-          },
-          {
-            title: "CSS",
-            iconLight: "",
-            iconDark: "images/icon/stacks/css-icon.png",
-          },
-          {
-            title: "Python",
-            iconLight: "",
-            iconDark: "images/icon/stacks/python-icon.png",
-          },
-          {
-            title: "JavaScript",
-            iconLight: "",
-            iconDark: "images/icon/stacks/js-icon.png",
-          },
-          {
-            title: "C Sharp",
-            iconLight: "",
-            iconDark: "images/icon/stacks/c-sharp-icon.png",
-          },
-          {
-            title: "SQL",
-            iconLight: "",
-            iconDark: "images/icon/stacks/sql-icon.png",
-          },
-          {
-            title: "Microsoft Azure",
-            iconLight: "",
-            iconDark: "images/icon/stacks/azure-icon.png",
-          },
-          {
-            title: "Figma",
-            iconLight: "",
-            iconDark: "images/icon/stacks/figma-2-icon.png",
-          },
-          {
-            title: "Lucidchart",
-            iconLight: "",
-            iconDark: "images/icon/stacks/lucidchart-2-icon.png",
-          },
-        ],
-        link: "",
-      },
-      {
-        title: "INTEX I - Social Media Usage and Mental Health Dashboard",
-        description: `This was the culminating project of the first semester of the BYU Information Systems junior core. As part of a team of four students that I had been working with throughout the semester on various projects, we developed a professional dashboard for the Provo City Council (fictional client) that showed the relationship between mental health and social media usage. Some of the main features we included were: \n
-		○ Form submission functionality for new people being surveyed \n
-		○ Admin functionalities for adding, editing, and removing data \n
-		○ Real-time update of dashboards as data was added, removed, or edited \n
-		○ Basic log in functionalities \n
-		○ Professional data visualizations following best industry practices
-`,
-        codingStacks: [
-          {
-            title: "HTML",
-            iconLight: "",
-            iconDark: "images/icon/stacks/html-icon.png",
-          },
-          {
-            title: "CSS",
-            iconLight: "",
-            iconDark: "images/icon/stacks/css-icon.png",
-          },
-          {
-            title: "Python",
-            iconLight: "",
-            iconDark: "images/icon/stacks/python-icon.png",
-          },
-          {
-            title: "JavaScript",
-            iconLight: "",
-            iconDark: "images/icon/stacks/js-icon.png",
-          },
-          {
-            title: "NodeJS",
-            iconLight: "",
-            iconDark: "images/icon/stacks/nodejs-icon.png",
-          },
-          {
-            title: "SQL",
-            iconLight: "",
-            iconDark: "images/icon/stacks/sql-icon.png",
-          },
-          {
-            title: "Amazon Web Services",
-            iconLight: "images/icon/stacks/aws-light-icon.png",
-            iconDark: "images/icon/stacks/aws-dark-2-icon.png",
-          },
-          {
-            title: "Lucidchart",
-            iconLight: "",
-            iconDark: "images/icon/stacks/lucidchart-2-icon.png",
-          },
-        ],
-        link: "",
-      },
-      {
-        title: "ToaPro",
-        description: `Following the Nexus development framework, I worked in a team of 60 students of my cohort to create an app for professors to use to grade the INTEX series. Some of the main features implemented were: \n
-		⚡ Project submission for student teams \n
-		⚡ Grid view of submissions for teaching assistants and professors \n
-		⚡ Grading functionality for graders, visible to students upon completion of the grading process
-`,
-        codingStacks: [
-          {
-            title: "HTML",
-            iconLight: "",
-            iconDark: "images/icon/stacks/html-icon.png",
-          },
-          {
-            title: "CSS",
-            iconLight: "",
-            iconDark: "images/icon/stacks/css-icon.png",
-          },
-          {
-            title: "C Sharp",
-            iconLight: "",
-            iconDark: "images/icon/stacks/c-sharp-icon.png",
-          },
-          {
-            title: "SQL",
-            iconLight: "",
-            iconDark: "images/icon/stacks/sql-icon.png",
-          },
-          {
-            title: "Figma",
-            iconLight: "",
-            iconDark: "images/icon/stacks/figma-2-icon.png",
-          },
-        ],
         link: "",
       },
     ],
