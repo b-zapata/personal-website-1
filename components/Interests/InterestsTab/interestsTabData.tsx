6bb22ed33c649352649ca9eb181be6953fdabd22
b-zapata <b.zapata.n@gmail.com>
2024-07-08 10:44:02 -0600

    testing new image sizes

--- components/Interests/InterestsTab/interestsTabData.tsx
+++ components/Interests/InterestsTab/interestsTabData.tsx
@@ -7,7 +7,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 1,
         title: "Piano",
-        imageDark: "/images/interests/piano-me.jpg",
+        imageDark: "/images/interests/piano-me_2.jpg",
         imageLight: "",
         description:
           "I started learning to play the piano when I was about 5. My mom was my first piano teacher, and she taught me mainly classical and church music. When I was 13 I joined the School of Music of Antofagasta, where I learned with professional musicians, attended master classes, and played as a soloist with the student orchestra. Today, I've expanded my repertoire to movie scores, jazz, pop, folk, and a few other music styles.",
@@ -16,7 +16,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 2,
         title: "Dancing",
-        imageDark: "/images/interests/dancing-1.jpg",
+        imageDark: "/images/interests/dancing-1_2.jpg",
         imageLight: "",
         description:
           "When I was a freshman at BYU, I took a social dance class and started attending the BYU Salsa and Country Swing Clubs. I knew right away that partner dancing would be one of my favorite hobbies for life. Since then, I have participated in competitions, choreographies, and served in different leadership positions, such as president of the BYU Salsa Club. My favorite styles are Salsa, Bachata, and West Coast Swing.",
@@ -25,7 +25,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 3,
         title: "Singing",
-        imageDark: "/images/interests/mco.jpg",
+        imageDark: "/images/interests/mco_2.jpg",
         imageLight: "",
         description:
           "I have been involved in choirs in my local church community since I was a child. When I was an exchange student during my senior year of high school, I joined Millennial Choirs and Orchestras. I participated in the youth choir for a year, and in the grand chorus for another year after I graduated high school. I also participated in the multicultural choir that would have sang at the April 2020 General Conference of The Church of Jesus Christ of Latter-day Saints, which went online because of COVID. I have also conducted choirs at my local church community and sang in a few ensambles with friends.",
@@ -39,7 +39,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 1,
         title: "Soccer",
-        imageDark: "/images/interests/soccer.jpg",
+        imageDark: "/images/interests/soccer_2.jpg",
         imageLight: "",
         description:
           "Growing up in Chile, soccer was always in some way a part of my life. I was in preschool when I played my first few games of soccer, and I haven't stopped playing with friends since then. I can play in any position, but I enjoy midfielder, center back, and goalkeeper the most because of the game vision and play building I can make.",
@@ -48,7 +48,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 2,
         title: "Chess",
-        imageDark: "/images/interests/chess.jpg",
+        imageDark: "/images/interests/chess_2.jpg",
         imageLight: "",
         description:
           "I learned how to play chess when I was 5 years old. I never got too into it until I was 14, when I started to play with some friends at my local church community. Then, during the COVID-19 pandemic, I began to play every day on Chess.com, and I reached my peak ranking of 1200. I still play frequently today.",
@@ -57,7 +57,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 3,
         title: "Swimming",
-        imageDark: "/images/interests/swimmer.jpg",
+        imageDark: "/images/interests/swimmer_2.jpg",
         imageLight: "",
         description:
           "My parents got me into swimming when I was 10 years old, shortly after I almost drown at a school party because I didn't know how to swim. I learned all four swimming styles, and began to train every day. During training I realized that I was really close to the Chilean freestyle 50m national record for my age, so I decided I wanted to do it competitively. However, they closed my city's olympic pool when I was 13 for repairs. I never got back into competitive swimming, but I do it now as a hobby and as a way to stay fit.",
@@ -66,7 +66,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 4,
         title: "Boxing",
-        imageDark: "/images/interests/boxing-gloves.jpg",
+        imageDark: "/images/interests/boxing-gloves_2.jpg",
         imageLight: "",
         description:
           "I got into boxing on May 2024. Even though I don't do sparring (boxing with someone else), I still have a lot of fun training. Boxing is a very fun way to release stress and stay fit! Did you know you can burn twice as many calories during a boxing workout session than during a regular gym session?",
@@ -80,7 +80,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 1,
         title: "Christianity",
-        imageDark: "/images/interests/mission-2.jpg",
+        imageDark: "/images/interests/mission-2_2.jpg",
         imageLight: "",
         description:
           "I have been a practicing Christian since I was a child. To me, true Christianity is shown through actions rather than words. Going out of my way to perform simple acts of kindness and service to those around me, starting by my family, is one of the best ways to show my beliefs. I have been actively involved in volunteer service in my local church communtiy since I was young, the most remarkable so far being my service for two years as a missionary for The Church of Jesus Christ of Latter-day Saints.",
@@ -89,7 +89,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 2,
         title: "Reading",
-        imageDark: "/images/interests/book.jpg",
+        imageDark: "/images/interests/book_2.jpg",
         imageLight: "",
         description:
           "My favorite book genres are self-improvement, drama, and religious. Some of the books I have read include The 7 Habits of Highly Effective People, How to Make Friends and Influence People, Donde Surgen las Sombras (Where Shadows Emerge), The Chronicles of Narnia, the Bible, and The Book of Mormon. Some of the books I would like to read soon are Atomic Habits and the Qu'ran.",
@@ -98,7 +98,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 3,
         title: "Suits (TV Series)",
-        imageDark: "/images/interests/suits-poster.jpg",
+        imageDark: "/images/interests/suits-poster_2.jpg",
         imageLight: "",
         description:
           "I love the fast pace and adrenaline of this show. My favorite character is Harvey Specter, and my favorite seasons are 1, 5, and 7.",
@@ -107,7 +107,7 @@ const interestsTabData: InterestTab[] = [
       {
         interestId: 4,
         title: "Rocket League",
-        imageDark: "/images/interests/rocket-league.jpg",
+        imageDark: "/images/interests/rocket-league_2.jpg",
         imageLight: "",
         description:
           "I got into Rocket League on my senior year of high school. I had a really hard time learning how to play to make it work, but eventually I managed to get to the Gold division",
