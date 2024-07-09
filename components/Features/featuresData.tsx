de14039fe53723317388309c4afc04889cd44306
b-zapata <b.zapata.n@gmail.com>
2024-07-09 09:52:30 -0600

    changed icons on what I do section homepage

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,19 +3,19 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/icon-01.svg",
+    icon: "/images/icon/icon-05.svg",
     title: "Full-Stack Development",
     description: "",
   },
   {
     id: 2,
-    icon: "/images/icon/icon-02.svg",
+    icon: "/images/icon/icon-01.svg",
     title: "Data Analysis",
     description: "",
   },
   {
     id: 3,
-    icon: "/images/icon/icon-03.svg",
+    icon: "/images/icon/icon-06.svg",
     title: "Machine Learning",
     description: "",
   },
