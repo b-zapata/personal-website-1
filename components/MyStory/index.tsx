790ffb1e4c9ce8cac3200680f8a608950cb18f61
b-zapata <b.zapata.n@gmail.com>
2024-07-12 16:42:00 -0600

    testing out new about one code

--- components/MyStory/index.tsx
+++ components/MyStory/index.tsx
@@ -92,6 +92,7 @@ const About = () => {
       </section>
 
       {/* <!-- ===== About One (Test) Start ===== --> */}
+      {/*}
       <section>
         <div className="mx-auto max-w-c-1235 overflow-hidden px-4 md:px-8 2xl:px-0">
           <div className="flex flex-col-reverse items-center gap-8 md:flex-row lg:gap-32.5">
@@ -128,13 +129,13 @@ const About = () => {
               </div>
               <div className="hidden md:block">
                 <Image
-                  src="./images/about/family_4.png"
+                  src="/images/about/me-casual_3.jpg"
                   alt="About"
                   className="dark:hidden"
                   fill
                 />
                 <Image
-                  src="./images/about/family_4.png"
+                  src="/images/about/me-casual_3.jpg"
                   alt="About"
                   className="hidden dark:block"
                   fill
@@ -174,7 +175,93 @@ const About = () => {
           </div>
         </div>
       </section>
+      {*/}
       {/* <!-- ===== About One (Test) End ===== --> */}
+
+      {/* <!-- ===== About One Start ===== --> */}
+      <section>
+        <div className="mx-auto max-w-c-1235 overflow-hidden px-4 md:px-8 2xl:px-0">
+          <div className="flex flex-col-reverse items-center gap-8 md:flex-row lg:gap-32.5">
+            <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  x: 20,
+                },
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 1, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_left relative mx-auto aspect-[588/526.5] w-full md:w-1/2"
+            >
+              <div className="mb-8 block md:hidden">
+                <Image
+                  src="/images/about/me-casual_3.jpg"
+                  alt="About"
+                  className="dark:hidden"
+                  fill
+                />
+                <Image
+                  src="/images/about/me-casual_3.jpg"
+                  alt="About"
+                  className="hidden dark:block"
+                  fill
+                />
+              </div>
+              <div className="hidden md:block">
+                <Image
+                  src="/images/about/me-casual_3.jpg"
+                  alt="About"
+                  className="dark:hidden"
+                  fill
+                />
+                <Image
+                  src="/images/about/me-casual_3.jpg"
+                  alt="About"
+                  className="hidden dark:block"
+                  fill
+                />
+              </div>
+            </motion.div>
+            <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0,
+                  x: -20,
+                },
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 1, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_right md:w-1/2"
+            >
+              <p>
+                I was born in Santiago, Chile on 2001. When I was 8, I moved to
+                Antofagasta, in the north of Chile, where I lived most of my
+                teenage years. When I was 16, I came to the United States as an
+                international exchange student and attended Liahona Preparatory
+                Academy, where I did my senior year and graduated high school.
+                At age 17 I started studying at BYU. I took classes from varying
+                fields and got familiar with different career paths, and on my
+                sophomore year, I finally decided on majoring in Information
+                Systems.
+              </p>
+              <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white"></h3>
+            </motion.div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- ===== About One End ===== --> */}
 
       {/* <!-- ===== About Two Start ===== --> */}
       <section>
