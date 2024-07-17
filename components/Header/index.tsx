52f97aa2d6f2ff8f890c24c8d2790a314ae74fee
b-zapata <b.zapata.n@gmail.com>
2024-07-16 18:26:46 -0600

    testing out new logos again

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -39,14 +39,14 @@ const Header = () => {
         <div className="flex w-full items-center justify-between xl:w-1/4">
           <a href="/">
             <Image
-              src="/images/logo/signature-dark_4.png"
+              src="/images/logo/signature-dark_5.png"
               alt="logo"
               width={119.03}
               height={30}
               className="hidden w-1/2 dark:block"
             />
             <Image
-              src="/images/logo/signature-light_4.png"
+              src="/images/logo/signature-light_5.png"
               alt="logo"
               width={119.03}
               height={30}
