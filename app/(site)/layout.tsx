03ed8a1148861d3f889fc67e510b78c71ecc1050
b-zapata <b.zapata.n@gmail.com>
2024-07-20 14:25:37 -0600

    testing out Google analytics code

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -6,7 +6,10 @@ import Lines from "@/components/Lines";
 import ScrollToTop from "@/components/ScrollToTop";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
+import Head from "next/head";
+import Script from "next/script";
 import "../globals.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 import ToasterContext from "../context/ToastContext";
@@ -18,6 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <title>Benjamin's Portfolio</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Built with Next.js and TypeScript" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
       <body className={`dark:bg-black ${inter.className}`}>
         <ThemeProvider
           enableSystem={false}
@@ -29,7 +38,24 @@ export default function RootLayout({
           <ToasterContext />
           {children}
           <ScrollToTop />
+          <Footer />
         </ThemeProvider>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-518FVBBRJJ`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-518FVBBRJJ');
+            `,
+          }}
+        />
       </body>
     </html>
   );
