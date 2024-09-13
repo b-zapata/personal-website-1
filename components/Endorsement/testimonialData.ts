98476d2e87e4f8e3c1e95efeaae8da93d4eac5a9
b-zapata <b.zapata.n@gmail.com>
2024-09-12 19:42:04 -0600

    removed two blank endorsements

--- components/Endorsement/testimonialData.ts
+++ components/Endorsement/testimonialData.ts
@@ -23,29 +23,4 @@ export const testimonialData: Testimonial[] = [
     image: tylerJarvis,
     content: `"Ben is professional and efficient at finding out what we need, and then he executes it promptly and well. He has also come up with a lot of great ideas for improving our website--not just its appearance, but also its effectiveness at meeting the neds of our users. Finally, he is fast at learning new things: he quickly mastered the practically-incomprehensible web development platform that our university requires us to use, despite its very counterintuitive interfaces. I really wish we could keep him, but like all students, he'll soon graduate and leave us. You definitely should hire him."`,
   },
-  {
-    id: 3,
-    name: "Aroldo Cavalcante",
-    designation:
-      "General Authority Seventy @ The Church of Jesus Christ of Latter-day Saints",
-    image: image2,
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
-  },
-  {
-    id: 4,
-    name: "Matt Bohn",
-    designation: "Founter @democompany",
-    image: image1,
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
-  },
-  {
-    id: 5,
-    name: "Steve Maroney",
-    designation: "Computer Support Manager @ BYU Math Department",
-    image: image2,
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
-  },
 ];
